82052935a579b021a55f2cd9003c39d890270051
sareh65 <sareh.na65@gmail.com>
2023-08-15 20:38:25 -0300

    create user class

---
+++ src/main/java/br/com/snn/apitest/domain/User.java
@@ -0,0 +1,21 @@
+package br.com.snn.apitest.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private Double weight;
+}
