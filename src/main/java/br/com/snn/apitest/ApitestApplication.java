abc1b880a989156543529ee01d2b01bf2f818623
sareh65 <sareh.na65@gmail.com>
2023-08-15 20:37:29 -0300

    start project

---
+++ src/main/java/br/com/snn/apitest/ApitestApplication.java
@@ -0,0 +1,12 @@
+package br.com.snn.apitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitestApplication.class, args);
+	}
+}
