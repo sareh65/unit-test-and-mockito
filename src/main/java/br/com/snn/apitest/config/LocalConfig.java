b0ccb91c5cb327c15fe0d7c578acfcfb845aacce
sareh65 <sareh.na65@gmail.com>
2023-08-17 18:23:00 -0300

    profile local test

---
+++ src/main/java/br/com/snn/apitest/config/LocalConfig.java
@@ -0,0 +1,23 @@
+package br.com.snn.apitest.config;
+
+import br.com.snn.apitest.domain.User;
+import br.com.snn.apitest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+    @Autowired
+    private UserRepository repository;
+    @Bean
+    public void startDB(){
+        User u1 = new User(null,"sareh","jhg@gg.mk","9887",12.03);
+        User u2 = new User(null,"sopo","olg@gg.mk","91227",29.00);
+        repository.saveAll(List.of(u1,u2));
+    }
+}
